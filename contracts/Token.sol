d97fd8d69503324d1c27c0cbee5585a979b9a3be
KirillNikoda <nikodakirill386@gmail.com>
2021-09-04 12:07:25 +0300

    added transfers and approve funcs

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -7,13 +7,12 @@ contract Token {
 	string public symbol = "DAPP";
 	uint256 public decimals = 18;
 	uint256 public totalSupply;
+	mapping(address => uint256) public balanceOf;
+	mapping(address => mapping(address => uint256)) public allowance;
 
 	// Events
 	event Transfer(address indexed from, address indexed to, uint256 value);
-
-	// Track balances
-	mapping(address => uint256) public balanceOf;
-
+	event Approval(address indexed owner, address spender, uint256 value);
 	// Send tokens
 
 	constructor() {
@@ -23,10 +22,31 @@ contract Token {
 
 	function transfer(address _to, uint256 _value) public returns (bool success) {
 		require(balanceOf[msg.sender] >= _value);
+		_transfer(msg.sender, _to, _value);	
+		return true;
+	}
+
+	function _transfer(address _from, address _to, uint256 _value) internal {
 		require(_to != address(0));
-		balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+		balanceOf[_from] = balanceOf[_from] - _value;
 		balanceOf[_to] = balanceOf[_to] + _value;
 		emit Transfer(msg.sender, _to, _value);
+	}
+
+	// Approve tokens
+	function approve(address _spender, uint256 _value) public returns (bool success) {
+		require(_spender != address(0));
+		allowance[msg.sender][_spender] = _value;
+		emit Approval(msg.sender, _spender, _value);
+		return true;
+	}
+
+	// Transfer from
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+		require(_value <= balanceOf[_from]);
+		require(_value <= allowance[_from][msg.sender]);
+		allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+		_transfer(_from, _to, _value);
 		return true;
 	}
 }
